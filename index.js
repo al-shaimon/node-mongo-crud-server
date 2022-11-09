8df6107005c48bcada86c1ca6ea6cdcefd7fcd8b
al-shaimon <alshaimon968@gmail.com>
2022-11-09 21:19:34 +0600

    65-2 (recap) Mongodb and Send User Data To The Server

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// user: dbUser2
+// pass: FzPvcOI8Pp7wzmTc
+
+const uri =
+  'mongodb+srv://dbUser2:FzPvcOI8Pp7wzmTc@cluster0.zeydczn.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const userCollection = client.db('nodeMongoCrud').collection('users');
+    const user = {
+      name: 'dancing test',
+      email: 'dancing@gmail.com',
+    };
+    const result = await userCollection.insertOne(user);
+    console.log(result);
+  } finally {
+  }
+}
+
+run().catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('Hello from node mongo crud server');
